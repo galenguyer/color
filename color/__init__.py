95e05c214b68dcd823b048f304fe5a89011ba92d
Galen Guyer <galen@galenguyer.com>
2021-04-04 23:46:41 -0400

    Update colors in real(ish) time

--- color/__init__.py
+++ color/__init__.py
@@ -43,20 +43,28 @@ def _send_favicon():
     return send_from_directory("static", "favicon.ico")
 
 
-@APP.route("/")
-def _index():
-    try: 
+def get_rgb():
+    try:
         red = int(r.get("red").decode("UTF-8"))
     except:
         red = 0
-    try: 
+    try:
         green = int(r.get("green").decode("UTF-8"))
     except:
         green = 0
-    try: 
+    try:
         blue = int(r.get("blue").decode("UTF-8"))
     except:
         blue = 0
+    return {"red": red, "green": green, "blue": blue}
+
+
+@APP.route("/")
+def _index():
+    colors = get_rgb()
+    red = colors["red"]
+    green = colors["green"]
+    blue = colors["blue"]
     total = red + green + blue
     if total == 0:
         total = 1
@@ -66,3 +74,21 @@ def _index():
     return render_template(
         "index.html", commit_hash=commit_hash, r=r_percent, g=g_percent, b=b_percent
     )
+
+
+@APP.route("/api/v1/red", methods=["POST"])
+def _api_v1_red():
+    r.incr("red")
+    return jsonify(get_rgb())
+
+
+@APP.route("/api/v1/green", methods=["POST"])
+def _api_v1_green():
+    r.incr("green")
+    return jsonify(get_rgb())
+
+
+@APP.route("/api/v1/blue", methods=["POST"])
+def _api_v1_blue():
+    r.incr("blue")
+    return jsonify(get_rgb())
